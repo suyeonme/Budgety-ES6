fd8cb4ef22d0f5b0cf5958679e5c59432825b4bf
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-11 21:48:10 +0900

    Set up eventlistener and get data from UI

--- src/app.js
+++ src/app.js
@@ -8,17 +8,66 @@ const budgetController = (function() {
 //////////////////////////////////////
 // UI Controller
 const UIController = (function() {
+    const DOMStrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn",
+        incomeContainer: ".income__list",
+        expenseContainer: ".expenses__list",
+        budgetLabel: ".budget__value",
+        incomeLable: ".budget__income--value",
+        expenseLabel: ".budget__expenses--value",
+        percentLabel: ".budget__expenses--percentage",
+        container: ".container"
+    };
 
+    return {
+        getDOMString: function() {
+            return DOMStrings;
+        },
+
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,                       // inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: parseFloat(document.querySelector(DOMStrings.inputValue).value)
+            };
+        },
+    };
 })();
 
 
 //////////////////////////////////////
 //  Controller
 const controller = (function(budgetCtrl, UICtrl) {
+    const setupEventListener = function() {
+        const DOM = UICtrl.getDOMString();
 
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
+        document.addEventListener('keypress', e => {
+            if (e.keyCode === 13 || e.which === 13) ctrlAddItem();
+        });
+    };
+
+    const ctrlAddItem = function() {
+        let input, newItem;
+
+        // Get input data 
+        input = UICtrl.getInput();
+
+        // Add the item to the budget controller
+
+        // Add the item to the UI 
+
+        // Clear input field
+            
+        // Calculate and update the budget
+        
+        }
 })(budgetController,UIController);
 
-controller.init();
+
 
 
